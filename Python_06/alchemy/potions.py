15cf27166540261f621562ccd7aeba9e8a7bf4c0
RochdAiman <rochdaiman@gmail.com>
2026-03-09 20:37:43 +0000

    finish

--- Python_06/alchemy/potions.py
+++ Python_06/alchemy/potions.py
@@ -1,8 +1,10 @@
 from .elements import create_fire, create_water, create_earth, create_air
+
 fire_result = create_fire()
 water_result = create_water()
 earth_result = create_earth()
 air_result = create_air()
+
 def healing_potion():
     return f'Healing potion brewed with {fire_result} and {water_result}'
 
