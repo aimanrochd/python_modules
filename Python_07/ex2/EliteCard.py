f430ef706d75ed11d04dc585fbbca9a8a55d1288
RochdAiman <rochdaiman@gmail.com>
2026-03-10 00:14:54 +0000

    Adding some new things

---
+++ Python_07/ex2/EliteCard.py
@@ -0,0 +1,14 @@
+from ex0.Card import Card
+from .Combatable import Combatable
+from .Magical import Magical
+
+
+class EliteCard(Card, Combatable, Magical):
+    def play(self, game_state: dict) -> dict:
+        ...
+
+    def attack(self, target) -> dict:
+        ...
+
+    def cast_spell(self, spell_name: str, targets: list) -> dict:
+        ...
---
+++ Python_07/ex2/main.py
@@ -0,0 +1,12 @@
+from ex0.Card import Card
+from .Combatable import Combatable
+from .Magical import Magical
+
+
+def main() -> None:
+    print('=== DataDeck Ability System ===\n')
+
+    card = Card()
+
+if __name__ == "__main__":
+    main()
